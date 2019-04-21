9ddebece8082172af670a146a72a2b0aec24af04
Sarah Kinneer <kinneers@gmail.com>
2019-04-21 14:47:35 -0400

    Added background photo to jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Jumbotron() {
     return (
-        <div className="jumbotron text-center">
+        <div className="jumbotron" id='jumbo'>
             <h1 className="display-4 mx-auto">(React) Google Books Search</h1>
             <p className="lead">Search for and Save Books of Interest</p>
         </div>
